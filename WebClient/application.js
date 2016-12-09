ef71a2c250029aef82a8e6058ef9bda065e3b4e2
kishila <s16g459@stu.kagawa-u.ac.jp>
2016-12-09 05:17:01 -0800

    button on POST message

--- WebClient/application.js
+++ WebClient/application.js
@@ -1,24 +1,38 @@
 (function (){
   window.onload = function(){
-    var msgbox = document.getElementById("msgs");
-    var green= document.getElementById('green');
+    var green = document.getElementById('green');
+    var red = document.getElementById('red');
+    var orange = document.getElementById('orange');
 
     green.addEventListener("click", function(){
-      var ws = new WebSocket('ws://' + "localhost");
-
-      ws.onopen = function() {
-        console.log("connection opened");
-        ws.send("test message");
-        ws.closed();
-      }
-      ws.onclose = function() {
-        console.log("connection closed");
-      }
-      ws.onmessage = function(m) {
-        var li = document.createElement("li");
-        li.textContent = m.data;
-        msgbox.insertBefore(li, msgbox.firstChild);
-      }
+      sendPOST("1");
     });
+
+    red.addEventListener("click", function(){
+      sendPOST("2");
+    });
+
+    orange.addEventListener("click", function(){
+      sendPOST("3");
+    });
+
+    var sendPOST = function(data) {
+      var url="http://localhost/"; // リクエスト先URL
+      //var data="test message"; // 送信データ ('param=value&...')
+      var request = new XMLHttpRequest();
+      request.open('POST', url);
+      request.onreadystatechange = function () {
+        if (request.readyState != 4) {
+          console.log("リクエスト中");
+        } else if (request.status != 200) {
+          console.log("失敗");
+        } else {
+          console.log("成功");
+          // var result = request.responseText;
+        }
+      };
+      request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+      request.send(data);
+    }
   }
 })();
